6ab338ee87e6ef30df514c4031f14d9698d41232
Tawambo <taracha@Tawambo-Creative.local>
2026-04-04 00:09:04 +0300

    Basic Variable Types

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -2,5 +2,9 @@
 pragma solidity 0.8.18;  // stating our solidity version
 
 contract SimpleStorage {
+    // Basic Types: boolean, uint, int, address, bytes
+    // Variables are holders for different values & these values can have one of these types
 
+    // favoriteNumber gets initialized to 0 if no value is given
+    uint256 favoriteNumber; // 0
 }
